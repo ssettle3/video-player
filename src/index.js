ebcce9d54149e68706996dd7777e45eeef8f5ec8
Stephen Settle <stephen.settle@salesloft.com>
2017-01-15 17:58:57 -0500

    Main app component and styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
 
-import App from './components/app';
-import reducers from './reducers';
+import YTSearch from "youtube-api-search";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = "AIzaSyAwQk5CkLE54_onKaSZHTF-EgIv5J_xQtM";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      seletedVideo: null
+    };
+
+    this.searchVideo("deathstar");
+  }
+
+  searchVideo(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ videos, selectedVideo: videos[0] });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.searchVideo(term) }, 500);
+
+    return (
+      <div>
+        <h1 className="header">YouTube - Except Not</h1>
+        <SearchBar onSearchVideo={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onSelect={(selectedVideo) => { this.setState({ selectedVideo }) }}
+          videos={this.state.videos} />
+      </div>
+    )
+  }
+};
+
+ReactDOM.render(<App />, document.querySelector(".container"));
